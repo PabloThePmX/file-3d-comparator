fb6b4fbf3c0ba504ee41e0fb3eb8ee73c1cc4b4b
Pablo <pablosarturi1@gmail.com>
2023-06-29 00:31:02 -0300

    solution and main files created for the stl file comparator

---
+++ STLFileComparator/InputFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STLFileComparator
+{
+    internal class InputFile
+    {
+        public static void GetPath()
+        {
+            string valor = Console.ReadLine();
+            Console.WriteLine($"Seu valor é {valor}");
+        }
+    }
+}
---
+++ STLFileComparator/Program.cs
@@ -0,0 +1,5 @@
+﻿using STLFileComparator;
+
+Console.WriteLine("Insira o caminho para a peça de referência");
+InputFile.GetPath();
+Console.ReadKey();
